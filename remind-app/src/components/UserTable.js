795bb9320c531f54f8c935713b420b6baed5aced
Colin <cmmatt14@gmail.com>
2022-12-05 19:47:26 -0500

    added fetch function to on click

--- remind-app/src/components/UserTable.js
+++ remind-app/src/components/UserTable.js
@@ -65,7 +65,7 @@ const UserTable = (props) => {
                         
                     </tbody>
                 </Table>
-                <button>Submit Group</button>
+                <button onClick={fetchSubmitGroup}>Submit Group</button>
             </Fragment>
 
   );
